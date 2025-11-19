e5c247625eeb683af43ee4eb609789433d84c61e
JFenderson <fenderson.joseph@gmail.com>
2025-11-19 09:00:26 -0600

    setting base files for setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: ['./tsconfig.base.json', './apps/*/tsconfig.json', './libs/*/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+  },
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    '.next',
+    'coverage',
+    '*.js',
+    '!.eslintrc.js',
+  ],
+};
